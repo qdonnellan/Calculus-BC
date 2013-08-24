35793dc3ea0d5560ed0126199b58f36a6143a20a
qdonnellan <quentin@Quentins-MacBook-Air.local>
2013-08-24 14:57:17 -0500

    made some changes to match lessonwell code

--- Problem Set 4/problem_4_5.py
+++ Problem Set 4/problem_4_5.py
@@ -1,9 +1,8 @@
-from __future__ import division
 from pylab import *
-
+ 
 def f(x):
-    return sin(x)/x
-
-x = linspace(-2,2,num=1000)
+    return -3*sin(x)/x + 4
+ 
+x = linspace(0.1,100,num=1000)
 plot(x,f(x))
 show()
